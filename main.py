99ace21856200d41fb3a0e8e38f0ad24cd165494
Rahul <rn2@williams.edu>
2014-11-22 16:38:26 -0500

    basic backend for main page and search page...I think. Didn't test.

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__medicost__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    error = None
+    if request.method == 'POST':
+        if valid_request(request.form['procedure'], request.form['zip']):
+            return search(request.form['username'])
+    else:
+        error = 'Invalid username/password'
+    return render_template('index.html', error = error)
+    
 
-if __name__ == "__main__":
-    app.run()
+def search(form_data):
+    print "send to providers page"
+    return redirect(url_for('search_results'))
+
+def valid_request():
+    print "valid request"
+
+@app.route('/providers/')
+def providers():
+    print "search page"
+
+#search function by procedure returns the list of provides per procedure
+
+if __medicost__ == '__main__':
+    app.run(debug = True)
+#    app.run(host='0.0.0.0')
