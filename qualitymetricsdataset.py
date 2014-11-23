e850ee2b9981e1164226035e9b8038a95f474976
state299 <itsnotmii299@gmail.com>
2014-11-23 12:50:01 -0500

    added stuff

---
+++ qualitymetricsdataset.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 23 10:23:46 2014
+
+@author: david
+"""
+qualitymetrics =  pd.read_csv("/home/david/Desktop/National_Downloadable_File.csv",header=1)
