3962d29340117e3495238342456298f02da06927
state299 <itsnotmii299@gmail.com>
2014-11-23 13:09:36 -0500

    Never going to give you up, never going to let you down, never going to run around and hurt you. - Rick Astely

--- main.py
+++ main.py
@@ -20,10 +20,11 @@ def search():
         return '500'
 
 
-def server_call(procedures, zip_code):
-    largeframe =  pd.read_csv("CSV_DATA_LOCATION",header=1)
-    results = largeframe.loc[(largeframe.nppes_provider_zip == zip_code) & (largeframe.hcpcs_description == procedures), ['nppes_provider_last_org_name', 'nppes_provider_first_name',"average_Medicare_allowed_amt","average_Medicare_payment_amt"]]
-    return  results.sort(['average_Medicare_allowed_amt', 'average_Medicare_payment_amt'], ascending=[1, 0])
+def server_call(procedures, state):
+    largeframe =  pd.read_csv("/home/david/Desktop/Medicare-Physician-and-Other-Supplier-PUF-yz-CY2012.csv",header=1)
+    results = largeframe.loc[(largeframe.nppes_provider_state == state) & (largeframe.hcpcs_description == procedures), ['nppes_provider_last_org_name', 'nppes_provider_first_name',"average_Medicare_allowed_amt","average_Medicare_payment_amt"]]
+    results = results.sort(['average_Medicare_allowed_amt', 'average_Medicare_payment_amt'], ascending=[1, 0])
+    return results.tojson()
 
 if __name__ == '__main__':
     app.run(debug = True, port=80)
